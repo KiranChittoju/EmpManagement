fab8274366445d0a17c8db1555032e45d5358d9f
krishnavamsi <krishnavamsipolepalli97pk@gmail.com>
2020-11-22 10:32:02 -0500

    Banks activity class update

--- app/src/main/java/com/example/employer/Banks.java
+++ app/src/main/java/com/example/employer/Banks.java
@@ -17,11 +17,50 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 public class Banks extends AppCompatActivity  {
 
-
+    TextView VacationPay;
+    TextView SickHrs;
+    FirebaseFirestore db;
+    String user_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banks);
+        user_id="EMP001";
+        VacationPay=findViewById(R.id.vacationPay);
+        SickHrs=findViewById(R.id.sickHrs);
+
+        db=FirebaseFirestore.getInstance();
+        getdata(user_id);
+    }
+
+    public void getdata(String User_id){
+
+        db.collection("employeeBanks")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            String vacation="";
+                            String sickness="";
+                            for(DocumentSnapshot document:task.getResult()){
+                                EmployeesBank emp_bank=document.toObject(EmployeesBank.class);
+                                if(user_id.equals(emp_bank.getUser_id())){
+                                    vacation=emp_bank.getVacation();
+                                    sickness=emp_bank.getSickness_hrs(); }
+                            }
+                            VacationPay.setText(vacation+".00");
+                            SickHrs.setText(sickness);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
+
 
     }
 }
