f1f58fe2fc7da14c76930c0f81ec34cbf3bd93ae
krishnavamsi <krishnavamsipolepalli97pk@gmail.com>
2020-11-22 10:25:50 -0500

    Banks model class updated

--- app/src/main/java/com/example/employer/Model/EmployeesBank.java
+++ app/src/main/java/com/example/employer/Model/EmployeesBank.java
@@ -2,5 +2,37 @@ package com.example.employer.Model;
 
 public class EmployeesBank {
 
+    String Vacation;
+    String Sickness_hrs;
+    String User_id;
 
+    public EmployeesBank(String vacation, String sickness_hrs, String user_id) {
+        Vacation = vacation;
+        Sickness_hrs = sickness_hrs;
+        User_id = user_id;
+    }
+
+    public String getVacation() {
+        return Vacation;
+    }
+
+    public void setVacation(String vacation) {
+        Vacation = vacation;
+    }
+
+    public String getSickness_hrs() {
+        return Sickness_hrs;
+    }
+
+    public void setSickness_hrs(String sickness_hrs) {
+        Sickness_hrs = sickness_hrs;
+    }
+
+    public String getUser_id() {
+        return User_id;
+    }
+
+    public void setUser_id(String user_id) {
+        User_id = user_id;
+    }
 }
